8536169c6e31c2417043654632265ea27d1cee38
student <jwang@vsu.edu>
2018-10-11 12:09:27 -0400

    first cmt

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+    std::string data = "y=5.9568x+3.14"; //say you have a string like this..
+
+    double y, x, m, b;
+    y = 0;
+    x = 0;
+
+    std::size_t offset = 0; //offset will be set to the length of characters of the "value" - 1.
+
+    std::cout<<"hello";
+    return 0;
+}
